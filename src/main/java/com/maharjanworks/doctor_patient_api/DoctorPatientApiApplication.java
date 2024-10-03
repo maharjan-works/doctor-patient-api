cab4c5d296bc5afe546083cd7cd5009da2680625
maharjan.works <maharjan.works@gmail.com>
2024-10-03 11:11:44 -0400

    base code

---
+++ src/main/java/com/maharjanworks/doctor_patient_api/DoctorPatientApiApplication.java
@@ -0,0 +1,13 @@
+package com.maharjanworks.doctor_patient_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maharjanworks/doctor_patient_api/DoctorPatientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharjanworks.doctor_patient_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPatientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
